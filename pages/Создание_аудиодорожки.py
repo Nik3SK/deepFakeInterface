3d7d7287cece1c9f2fd4624a12b7a515b2ffe65c
Nick3SK <skopin.nicita654@yandex.ru>
2023-11-16 16:59:53 +0300

    first part of interface

---
+++ pages/Создание_аудиодорожки.py
@@ -0,0 +1,19 @@
+import os
+import streamlit as st
+import time
+from streamlit_extras.switch_page_button import switch_page
+os.chdir('C:/tts3_docker_16_10')
+st.set_page_config(page_title='Создание аудиодорожки')
+uplode_text = st.file_uploader('Загрузи конспект', ['txt','docx'])
+if (uplode_text is not None):
+    with open ('C:/tts3_docker_16_10/text.txt','wb') as f:
+        f.write(uplode_text.read())
+    st.subheader('Вы успешно загрузили файл, модель уже начала генерировать аудио....')
+    with st.spinner('Работает модель'):
+        os.system('docker compose up')
+    with open('output.wav','rb') as result_file:
+        st.subheader('Модель закончила работу. Можете посмотреть и скачать вашу лекцию')
+        st.download_button(label = 'Аудиодорожка',data = result_file,file_name='output.wav')
+        st.audio(result_file,format='wav')
+if st.button(label = 'К предыдущему шагу'):
+    switch_page('Знакомство')
---
+++ Знакомство.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import time
+st.set_page_config(page_title="Знакомство")
+from streamlit_extras.switch_page_button import switch_page
+st.title('Генерация видеолекций на основе фото, аудио и конспекта')
+st.subheader('На данный момент реализован функционал превращения текста в аудидорожку')
+if st.button(label='Хочу попробовать',type='primary'):
+    switch_page('Создание_аудиодорожки')
+# uplode_audio = st.file_uploader('Загрузи аудио', ['mp3'])
+# uplode_text = st.file_uploader('Загрузи конспект', ['txt','docx'])
+# if (uplode_audio is not None) or (uplode_text is not None):
+#     st.subheader('Вы успешно загрузили все файлы, модель уже начала генерировать видео....')
+#     time.sleep(5)
+#     result_file = open('Mot.mp4','rb')
+#     result_play = result_file.read()
+#     st.subheader('Модель закончила работу. Можете посмотреть и скачать вашу лекцию')
+#     st.download_button('Видеолекция','Mot.mp4')
+#     st.video(result_play)
