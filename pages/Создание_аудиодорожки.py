f5343a9a3ba48beb751d2d0129d99ee96e4947c5
Nick3SK <skopin.nicita654@yandex.ru>
2024-03-10 20:03:05 +0300

    add functionality of RVC without testing

--- pages/Создание_аудиодорожки.py
+++ pages/Создание_аудиодорожки.py
@@ -3,27 +3,79 @@ import streamlit as st
 import time
 from streamlit_extras.switch_page_button import switch_page
 import tempfile
+import tqdm
 
 
-@st.cache_data(show_spinner="Работает модель",ttl=3)
+@st.cache_data(show_spinner="Работает модель")
 def runningModel(text):
-    os.system('docker compose up')
+    st.info(st.session_state['info_for_user1'])
+    time.sleep(3)
+    # os.system('docker compose up')
+
+
+
 
 # Путь модели tts
-os.chdir(r"C:\Users\Edit-PC2\Documents\DeepFake\models\tts3_docker_16_10")
+os.chdir(r"C:\tts3_docker_16_10")
 st.set_page_config(page_title='Создание аудиодорожки')
-st.subheader('Загрузи конспект')
+st.title('Генерация аудиодорожки')
+st.divider()
+st.header('Модель text-to-speech преобразует файл с текстом в аудио.'+
+          'На данном этапе генерация возможна только с голосом одного спикера мужского пола')
+st.divider()
+st.subheader('Загрузи тексовый файл, к примеру конспект лекции')
 uplode_text = st.file_uploader('',['txt'])
+st.session_state['castomize']='no'
 if (uplode_text is not None):
-    with open (r'C:\Users\Edit-PC2\Documents\DeepFake\models\tts3_docker_16_10\text.txt','wb') as f:
+    with open (r'C:\tts3_docker_16_10\text.txt','wb') as f:
         f.write(uplode_text.read())
-    st.subheader('Вы успешно загрузили файл, модель уже начала генерировать аудио....')
+    st.session_state['info_for_user1']='Вы успешно загрузили файл, модель уже начала генерировать аудио....'
+    st.session_state['info_for_user2']='Модель закончила работу. Можете послушать и скачать вашу аудиодорожку'
     runningModel(uplode_text.name)
-    os.system('ffmpeg -i output.wav result.mp3')
-    with open('result.mp3','rb') as result_file:
-        st.subheader('Модель закончила работу. Можете посмотреть и скачать вашу лекцию')
+    st.success(st.session_state['info_for_user2'])
+    with open('output.wav','rb') as result_file:
         st.download_button(label = 'Аудиодорожка',data = result_file,file_name='output.wav')
         st.audio(result_file,format='wav')
+    st.session_state['castomize']='yes'
+    st.divider()
+if st.session_state['castomize']=='yes':
+    st.subheader('Теперь можно кастомизировать аудиодорожку')
+    os.chdir(r"C:\Users\Никита\Desktop\interface\deepFakeInterface\weights")
+    all_weights_row=os.listdir()
+    all_weights=[]
+    for i in all_weights_row:
+        all_weights.append(i[:i.find('.pth'):])
+    col1, col2= st.columns(2,gap="large")
+    with col1:
+        st.subheader("Выберите модель для кастомизации:")
+        option = st.selectbox('Список моделей', all_weights)
+
+    with col2:
+        st.subheader("Кастомизировать полученной аудио?")
+        if option!=None and st.button('Да', type='primary',use_container_width=True):
+            #Путь к папке с индексами
+            os.chdir(fr'C:\Users\Никита\Desktop\interface\deepFakeInterface\indexes\{option}')
+            index_file=os.listdir()[0]
+            #Путь к папке с RVC
+            # os.chdir()
+            os.system('python tools\infer_cli.py --f0up_key=0 '
+                    #   путь к результирующему файлу tts
+                      '--input_path=""'
+                    #   путь к индекс файлам
+                      ' --index_path="C:\Users\Edit-PC2\Documents\DeepFake\examplesForModels\weightsForRVC\\'+option+'.pth'+ '\\'+index_file+ '"'
+                    # путь для конечного, кастомизированного файла
+                      '--opt_path="test_v2.wav" '
+                      '--model_name="' + option +'" '
+                      ' --index_rate=0.66 --device=cuda:0 '
+                      '--is_half=True '
+                      '--filter_radius=3 '
+                      '--resample_sr=0 '
+                      '--rms_mix_rate=1'
+                      ' --protect=0.33')
+        else:
+            st.error('Ты не выбрал модель')
+    
+
+
 if st.button(label = 'К следующему шагу'):
-    os.remove('result.mp3')
     switch_page('Создание_видео')
